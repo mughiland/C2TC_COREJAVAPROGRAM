f7db2a9990587428bde6a699b58be76c750d91c3
Mughilan <Mughilan@DESKTOP-0F4OG71>
2025-07-21 23:05:46 +0530

    day two

--- C2TC_corejavaprogram/src/Daytwo/java/While.java
+++ C2TC_corejavaprogram/src/Daytwo/java/While.java
@@ -3,8 +3,17 @@ package Daytwo.java;
 public class While {
 
 	public static void main(String[] args) {
+		
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
 		// TODO Auto-generated method stub
 
 	}
 
-}
+
