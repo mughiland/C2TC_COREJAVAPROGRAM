799c45c50921947d34e645757304672a85c30e76
Mughilan <Mughilan@DESKTOP-0F4OG71>
2025-07-21 22:20:23 +0530

    class datatypes identifiers primitivedata

---
+++ C2TC_corejavaprogram/src/Dayone/Firstprogram.java
@@ -0,0 +1,12 @@
+package Dayone;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
