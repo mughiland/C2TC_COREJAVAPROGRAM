5ea87a2e0297c11162b522bf07633bc7a95d9f64
Mughilan <Mughilan@DESKTOP-0F4OG71>
2025-07-24 23:45:40 +0530

    inheritance

---
+++ C2TC_corejavaprogram/src/Dayfive/Hierarchicalinheritance/Employee.java
@@ -0,0 +1,30 @@
+package Dayfive.Hierarchicalinheritance;
+
+public class Employee extends Person {
+	private int empId;
+	private float salary;
+	private String dept;
+	
+	
+	public Employee() {
+		System.out.println("Employee Class Default Constructor");
+	}
+
+
+	public Employee(int empId, float salary, String dept) {
+		this.empId = empId;
+		this.salary = salary;
+		this.dept = dept;
+	}
+	
+	
+	public Employee(String name, String city,int empId, float salary, String dept) {
+		super(name,city);
+		this.empId = empId;
+		this.salary = salary;
+		this.dept = dept;
+	}
+
+}
+
+    
